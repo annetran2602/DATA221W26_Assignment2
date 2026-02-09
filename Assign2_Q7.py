e1f84fed3eebc763af6822dfb5b01f679a39d346
annetran2602 <anne.tran2@ucalgary.ca>
2026-02-08 19:02:34 -0700

    Assign2_Q7: Access to the website, extract the page title and the first paragraph which has at least 50 characters

---
+++ Assign2_Q7.py
@@ -0,0 +1,32 @@
+# Anne Tran (UCID: 30286177)
+# Assign2_Q7
+
+from bs4 import BeautifulSoup
+import requests
+
+
+headers={"User-Agent": "Mozilla/5.0"} # prevent website block the request from accessing the website
+
+# Scrape webpage
+web_html=requests.get("https://en.wikipedia.org/wiki/Data_science", headers=headers)
+parsed_web_html=BeautifulSoup(web_html.text, "html5lib")
+
+# Extract the page title
+titlePage=parsed_web_html.find('title') # find the title with 'title' tag
+
+print(f"Page title is {titlePage}") # display page title
+
+# Extract the first paragraph
+content_div=parsed_web_html.find("div", id="mw-content-text") # find main content
+                                                              # content_div indicates the main content
+paragraph=content_div.find_all("p") # finds all paragraph
+
+firstParagraph=""
+for p in paragraph:
+    text=p.text.strip() # get text from paragraph
+                        # remove whitespace
+    if len(text)>=50: # check if paragraph contain at least 50 characters
+        firstParagraph=text
+        break
+
+print(firstParagraph) # display the valid first paragraph
